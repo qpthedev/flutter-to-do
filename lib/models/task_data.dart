d276acbc83f0b24f76c117c472c1fbe2caed408c
qpthedev <qpthedev@gmail.com>
2021-10-04 03:33:34 -0400

    Add methods to modify tasks list

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,15 +1,37 @@
+import 'dart:collection';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_to_do/models/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Task A'),
     Task(name: 'Task B'),
     Task(name: 'Task C'),
   ];
 
-  // getter for task list
+  // prevent adding or deleting via List methods
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  // getter for task list length
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTitle) {
+    final task = Task(name: newTaskTitle);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
   }
 }
